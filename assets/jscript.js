8cbec78ffedc7b99f556ca75dfabfb7501116adb
unknown <pavankumar.23122007@gmail.com>
2024-06-19 05:49:05 -0700

    up

---
+++ assets/jscript.js
@@ -0,0 +1,8 @@
+function hamburg() {
+    var x = document.getElementById("navlist");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
